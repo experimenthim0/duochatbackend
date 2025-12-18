0b45b01eae16a7d7343d52a5913bf51f9d4aa364
experimenthim0 <contact.nikhim@gmail.com>
2025-12-18 18:08:19 +0530

    Consolidate server and Socket.io logic
    Refactor server setup and Socket.io connection handling for clarity.

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
 const express = require('express');
 const http = require('http');
-const app = express();
-const { Socket } = require('dgram');
+require("dotenv").config();
+const { Server } = require('socket.io');
 
-const {Server} = require('socket.io');
+const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+
+// REMOVED: app.listen(PORT); <--- This was the culprit!
+
+// 1. Create the HTTP server using the Express 'app'
 const server = http.createServer(app);
 
-
+// 2. Attach Socket.io to that HTTP server
 const io = new Server(server);
-
-
-
 
 app.use(express.static('public'));
 
-// Socket.io connection
-io.on('connection',(socket)=>{
-     socket.on('chatMessage',(msg)=>{
-        
-        io.emit('chatMessage',msg);
+// 3. Consolidate your Socket.io logic into one 'connection' block (cleaner)
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-     })
+  socket.on('chatMessage', (msg) => {
+    io.emit('chatMessage', msg);
+  });
 
-     
-    })
-    io.on('connection',(socket)=>{
-      
-      socket.on("imageMessage", (data) => {
-        // send to all users
-        io.emit("imageMessage", data);
-      });
-    })
-
-
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  socket.on("imageMessage", (data) => {
+    io.emit("imageMessage", data);
+  });
 });
 
+// 4. Start the server
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
